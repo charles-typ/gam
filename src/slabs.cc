d3f29f4d75bc84e86b7f1680b56c0621c601bc0e
Yupeng Tang <yupeng.tang@yale.edu>
2021-01-13 21:52:55 +0800

    Minor fix

--- src/slabs.cc
+++ src/slabs.cc
@@ -258,7 +258,7 @@ int SlabAllocator::do_slabs_newslab(const unsigned int id) {
 
   p->slab_list[p->slabs++] = ptr;
   mem_malloced += len;
-  epicLog(LOG_WARNING, "new slab class %d allocated, mem limit: %d, mem malloced: %d, len: %d, slabs: %d, item_size_max:", id, mem_limit, mem_malloced, len, p->slabs, item_size_max);
+  epicLog(LOG_WARNING, "new slab class %d allocated, mem limit: %d, mem malloced: %d, len: %d, slabs: %d, item_size_max: %d", id, mem_limit, mem_malloced, len, p->slabs, item_size_max);
   MEMCACHED_SLABS_SLABCLASS_ALLOCATE(id);
 
   return 1;
