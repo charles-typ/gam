596eab2b5841a4a08fe9d430fb933b318f45da4b
Yupeng Tang <yupeng.tang@yale.edu>
2021-04-28 01:46:39 -0400

    Update remote statistics

--- test/gam_profile_test.cc
+++ test/gam_profile_test.cc
@@ -326,6 +326,7 @@ void do_log(void *arg) {
       alloc->CollectNetworkCdf(trace->tid, trace->pass);
       alloc->CollectEvictStatistics(trace->tid, trace->pass);
       alloc->CollectInvalidStatistics(trace->tid, trace->pass);
+      alloc->CollectRemoteStatistics(trace->tid, trace->pass);
     }
     //if(trace->read_ops)
     //  printf("total read time is %ld ns, thread: %d, pass: %d\n", trace->read_time, trace->tid, trace->pass);
